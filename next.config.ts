e2ef4cd7583e56c9c9c58d614fd19f5687062283
B-grande <101441595+B-grande@users.noreply.github.com>
2025-03-12 20:47:49 -0400

    fixed basepath + a deploy to hithub pageq

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // enables a static export
+  basePath: "/B-grande.github.io", // Only my github repo
+  assetPrefix: "/B-grande.github.io/", //match repo name
 };
 
 export default nextConfig;
