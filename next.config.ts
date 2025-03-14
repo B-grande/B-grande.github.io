ec7c424a26aa58445c3c42f7780816dfe82a61ed
Brian Grande <101441595+B-grande@users.noreply.github.com>
2025-03-14 18:18:47 -0400

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export", // enables a static export
-  basePath: "/B-grande.github.io", // Only my github repo
-  assetPrefix: "/B-grande.github.io/", //match repo name
-};
-
-export default nextConfig;
